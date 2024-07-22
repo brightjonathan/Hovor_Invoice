2cc9f2b15057872c6ec09e85b1f2805f2c181620
Bright Jonathan <brightjonathan000@gmail.com>
2024-07-22 01:53:11 +0100

    added more func...

--- src/Pages/Invoice.jsx
+++ src/Pages/Invoice.jsx
@@ -1,14 +1,44 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { State } from "../Context/StateContext";
 
 
 const Invoice = ({isAuth}) => {
 
+    const {
+        
+      } = useContext(State);
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const navigate = useNavigate();    
 useEffect(()=>{
     if(!isAuth){
-      navigate('/login')
+      navigate('/signin')
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuth]);
@@ -16,9 +46,222 @@ useEffect(()=>{
 
 
   return (
-    <div>
-      <h2> Invoice page  </h2>
-    </div>
+    <>
+      <main
+        className="m-5 p-5 xl:grid grid-cols-2 gap-10 xl:items-start"
+        style={{
+          maxWidth: "1920px",
+          margin: "auto",
+        }}
+      >
+        <section>
+          <div className="bg-white p-5 rounded shadow">
+            <div className="flex flex-col justify-center">
+              <article className="md:grid grid-cols-2 gap-10">
+                <div className="flex flex-col">
+                  <label htmlFor="name">Your full name</label>
+                  <input
+                    type="text"
+                    name="text"
+                    id="name"
+                    placeholder="Enter your name"
+                    maxLength={56}
+                    autoComplete="off"
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="address">Enter your address</label>
+                  <input
+                    type="text"
+                    name="address"
+                    id="address"
+                    placeholder="Enter your address"
+                    autoComplete="off"
+                    maxLength={96}
+                  />
+                </div>
+              </article>
+
+              <article className="md:grid grid-cols-3 gap-10">
+                <div className="flex flex-col">
+                  <label htmlFor="email">Enter your email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Enter your email"
+                    maxLength={255}
+                    autoComplete="off"
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="website">Enter your website</label>
+                  <input
+                    type="url"
+                    name="website"
+                    id="website"
+                    placeholder="Enter your website"
+                    maxLength={96}
+                    autoComplete="off"
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="phone">Enter your phone</label>
+                  <input
+                    type="text"
+                    name="phone"
+                    id="phone"
+                    placeholder="Enter your phone"
+                    maxLength={12}
+                    autoComplete="off"
+
+                  />
+                </div>
+              </article>
+
+              <article className="md:grid grid-cols-2 gap-10">
+                <div className="flex flex-col">
+                  <label htmlFor="bankName">Enter your bank name</label>
+                  <input
+                    type="text"
+                    name="bankName"
+                    id="bankName"
+                    placeholder="Enter your bank name"
+                    maxLength={56}
+                    autoComplete="off"
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="bankAccount">
+                    Enter your bank account number
+                  </label>
+                  <input
+                    type="text"
+                    name="bankAccount"
+                    id="bankAccount"
+                    placeholder="Enter your bank account number"
+                    maxLength={20}
+                    autoComplete="off"
+                  />
+                </div>
+              </article>
+
+              <article className="md:grid grid-cols-2 gap-10 md:mt-16">
+                <div className="flex flex-col">
+                  <label htmlFor="clientName">Enter your client's name</label>
+                  <input
+                    type="text"
+                    name="clientName"
+                    id="clientName"
+                    placeholder="Enter your client's name"
+                    maxLength={56}
+                    autoComplete="off"
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="clientAddress">
+                    Enter your client's address
+                  </label>
+                  <input
+                    type="text"
+                    name="clientAddress"
+                    id="clientAddress"
+                    placeholder="Enter your client's address"
+                    maxLength={96}
+                    autoComplete="off"
+                  />
+                </div>
+              </article>
+
+              <article className="md:grid grid-cols-3 gap-10">
+                <div className="flex flex-col">
+                  <label htmlFor="invoiceNumber">Invoice Number</label>
+                  <input
+                    type="text"
+                    name="invoiceNumber"
+                    id="invoiceNumber"
+                    placeholder="Invoice Number"
+                    autoComplete="off"
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="invoiceDate">Invoice Date</label>
+                  <input
+                    type="date"
+                    name="invoiceDate"
+                    id="invoiceDate"
+                    placeholder="Invoice Date"
+                    autoComplete="off"
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <label htmlFor="dueDate">Due Date</label>
+                  <input
+                    type="date"
+                    name="dueDate"
+                    id="dueDate"
+                    placeholder="Invoice Date"
+                    autoComplete="off"
+                  />
+                </div>
+              </article>
+
+              {/* This is our table form */}
+              <article>
+                {/* <TableForm /> */}
+              </article>
+
+              <label htmlFor="notes">Additional Notes</label>
+              <textarea
+                name="notes"
+                id="notes"
+                cols="30"
+                rows="10"
+                placeholder="Additional notes to the client"
+                maxLength={500}
+              ></textarea>
+            </div>
+          </div>
+          <article className="mt-5">
+            {/* <DonateButton /> */}
+          </article>
+        </section>
+
+        {/* Invoice Preview */}
+        <div className="invoice__preview bg-white p-5 rounded-2xl border-4 border-blue-200">
+          {/* <ReactToPrint
+            trigger={() => (
+              <button className="bg-blue-500 ml-5 text-white font-bold py-2 px-8 rounded hover:bg-blue-600 hover:text-white transition-all duration-150 hover:ring-4 hover:ring-blue-400">
+                Print / Download
+              </button>
+            )}
+            content={() => componentRef.current}
+          /> */}
+          <div  className="p-5">
+            {/* <Header />
+
+            <MainDetails />
+
+            <ClientDetails />
+
+            <Dates />
+
+            <Table />
+
+            <Notes />
+
+            <Footer /> */}
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
 
